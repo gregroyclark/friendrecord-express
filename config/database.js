015a8246e437e17df65c51952fffb76df207056c
gregroyclark <gregroyclark@gmail.com>
2024-01-08 05:27:35 -0800

    updated prod db config

--- config/database.js
+++ config/database.js
@@ -3,30 +3,39 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // // local
-const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+// const db = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   port: Number(process.env.MYSQL_PORT),
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+// });
 
 // prod
-// const db = mysql.createPool({
-//   database: process.env.DATABASE,
-//   host: process.env.PLANETSCALE_DB_HOST,
-//   user: process.env.PLANETSCALE_DB_USERNAME,
-//   password: process.env.PLANETSCALE_DB_PASSWORD,
-//   port: Number(process.env.MYSQL_PORT),
-// });
+const db = mysql.createPool({
+  database: process.env.DATABASE_URL,
+  // database: process.env.DATABASE,
+  // host: process.env.PLANETSCALE_DB_HOST,
+  // user: process.env.PLANETSCALE_DB_USERNAME,
+  // password: process.env.PLANETSCALE_DB_PASSWORD,
+  // port: Number(process.env.MYSQL_PORT),
+});
 
 db.getConnection((error, connection) => {
   try {
     connection.release();
     console.log('Connected to the database');
-  } catch (error) {
-    throw new Error('Error connecting to the database: ', error);
+  } catch (err) {
+    if (err) {
+      throw new Error('Error connecting to the database: ', error);
+    }
   }
 });
+
+// db.getConnection((err, connection) => {
+//   if (err) throw err;
+//   console.log('Connected to the database');
+//   connection.release();
+// });
 
 module.exports = { db };
