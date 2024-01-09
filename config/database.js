7924bc17603723e2bdd6d6de3d6d20911790d645
gregroyclark <gregroyclark@gmail.com>
2024-01-09 04:19:42 -0800

    code works with just uri

--- config/database.js
+++ config/database.js
@@ -3,39 +3,56 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // // local
-// const db = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   port: Number(process.env.MYSQL_PORT),
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-// });
+const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  // host: process.env.MYSQL_HOST,
+  // port: Number(process.env.MYSQL_PORT),
+  // user: process.env.MYSQL_USER,
+  // password: process.env.MYSQL_PASSWORD,
+  // database: process.env.MYSQL_DATABASE,
+});
 
 // prod
-const db = mysql.createPool({
-  database_url: process.env.PLANETSCALE_DATABASE_URL,
-  database: process.env.DATABASE,
-  host: process.env.PLANETSCALE_DB_HOST,
-  user: process.env.PLANETSCALE_DB_USERNAME,
-  password: process.env.PLANETSCALE_DB_PASSWORD,
-  port: Number(process.env.MYSQL_PORT),
-});
-
-db.getConnection((error, connection) => {
-  try {
-    connection.release();
-    console.log('Connected to the database');
-  } catch (err) {
-    if (err) {
-      throw new Error('Error connecting to the database: ', error);
-    }
-  }
-});
-
-// db.getConnection((err, connection) => {
-//   if (err) throw err;
-//   console.log('Connected to the database');
-//   connection.release();
+// const db = mysql.createPool({
+//   url: process.env.DATABASE_URL,
+//   database: process.env.DATABASE,
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   port: Number(process.env.MYSQL_PORT),
 // });
+
+// db.getConnection((error, connection) => {
+//   try {
+//     connection.release();
+//     console.log('Connected to the database');
+//   } catch (err) {
+//     if (err) {
+//       console.error('Error connecting to the database: ', err);
+//       throw new Error('Error connecting to the database: ', error);
+//     }
+//   }
+// });
+
+// const testConnection = async () => {
+//   try {
+//     const connection = await db.getConnection();
+//     console.log('Connected to the database');
+//     connection.release();
+//   } catch (error) {
+//     console.error('Error connecting to the database: ', error);
+//     throw new Error('Error connecting to the database: ', error);
+//   }
+// };
+
+// testConnection();
+
+db.getConnection((err, connection) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to the database');
+  connection.release();
+});
 
 module.exports = { db };
