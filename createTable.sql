25d385930da116908ab28b5d9141b10710a4dfa2
gregroyclark <gregroyclark@gmail.com>
2024-01-09 18:07:07 -0800

    grants subuser role

--- createTable.sql
+++ createTable.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE friendsdb;
+GRANT postgres_subuser1 TO postgres;
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
   lastName VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
+  hashedPassword VARCHAR(255) NOT NULL,
   userId VARCHAR(255) UNIQUE NOT NULL
 );
 
