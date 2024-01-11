e871b48f933fdea6e3cc5607394013a41826bd28
gregroyclark <gregroyclark@gmail.com>
2024-01-11 02:04:44 -0800

    added quoted identifiers

--- createTable.sql
+++ createTable.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  hashedPassword VARCHAR(255) NOT NULL,
-  userId VARCHAR(255) UNIQUE NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "firstName" VARCHAR(255) NOT NULL,
+  "lastName" VARCHAR(255) NOT NULL,
+  "email" VARCHAR(255) UNIQUE NOT NULL,
+  "hashedPassword" VARCHAR(255) NOT NULL,
+  "userId" VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS friends (
-  id SERIAL PRIMARY KEY,
-  firstName VARCHAR(255),
-  lastName VARCHAR(255),
-  email VARCHAR(255),
-  phoneNumber VARCHAR(255),
-  notes TEXT,
-  userId VARCHAR(255) UNIQUE NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "firstName" VARCHAR(255),
+  "lastName" VARCHAR(255),
+  "email" VARCHAR(255),
+  "phoneNumber" VARCHAR(255),
+  "notes" TEXT,
+  "userId" VARCHAR(255) UNIQUE NOT NULL
 );
